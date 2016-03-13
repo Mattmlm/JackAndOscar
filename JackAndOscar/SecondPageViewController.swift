3d4452dbe94288e065cc73ef8357122e39d27219
Ashkhen Sargsyan <ashi@a5.io>
2016-03-13 15:02:08 -0700

    Added additional images for eyelid and improved code for blinking animation

--- JackAndOscar/SecondPageViewController.swift
+++ JackAndOscar/SecondPageViewController.swift
@@ -14,6 +14,8 @@ class SecondPageViewController: UIViewController {
     
     var image_1: UIImage!
     var image_2: UIImage!
+    var image_3: UIImage!
+    var image_4: UIImage!
     
     var images: [UIImage]!
    
@@ -23,10 +25,12 @@ class SecondPageViewController: UIViewController {
         super.viewDidLoad()
         
         image_1 = UIImage(named: "transparent")
-        image_2 = UIImage(named: "jack2Eyelids")
+        image_2 = UIImage(named: "jack2Eyelids_halfopen")
+        image_3 = UIImage(named: "jack2Eyelids_halfopen_2")
+        image_4 = UIImage(named: "jack2Eyelids")
         
-        images = [image_1, image_2]
-        animatedImage = UIImage.animatedImageWithImages(images, duration: 0.6)
+        images = [image_1, image_2, image_3, image_4]
+        animatedImage = UIImage.animatedImageWithImages(images, duration:0.6)
     
         // Do any additional setup after loading the view.
     }
