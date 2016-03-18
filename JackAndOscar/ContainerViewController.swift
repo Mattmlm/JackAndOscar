bdccf2b25335a747bfd82bdbbdab920f97da7266
Ashkhen Sargsyan <ashi@a5.io>
2016-03-18 15:17:57 -0700

    Added container view for menu and audio buttons

---
+++ JackAndOscar/ContainerViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ContainerViewController.swift
+//  JackAndOscar
+//
+//  Created by Ashkhen Sargsyan on 3/18/16.
+//  Copyright © 2016 Ashkhen Sargsyan. All rights reserved.
+//
+
+import UIKit
+
+class ContainerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func onDismissTap() {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
