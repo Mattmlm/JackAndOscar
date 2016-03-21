ff67b4740f5207a1719df50181b194d6ca17a5d6
Ashkhen Sargsyan <ashi@a5.io>
2016-03-20 20:29:56 -0700

    Added code for voice recording.

--- JackAndOscar/ContainerViewController.swift
+++ JackAndOscar/ContainerViewController.swift
@@ -33,6 +33,41 @@ class ContainerViewController: UIViewController {
         }
     }
     
+    var audioRecorder: AVAudioRecorder!
+    
+    func record() {
+        let audioSession: AVAudioSession = AVAudioSession.sharedInstance()
+        
+        if (audioSession.respondsToSelector("requestToRecordPermission:")) {
+            AVAudioSession.sharedInstance().requestRecordPermission({(granted: Bool) -> Void in
+                if granted {
+                    print("granted")
+                    
+                    try! audioSession.setCategory(AVAudioSessionCategoryPlayAndRecord)
+                    try! audioSession.setActive(true)
+                    
+                    let documentsDirectory = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0]
+                    let fullPath = documentsDirectory.stringByAppendingString("voiceRecording.caf")
+                    let url = NSURL.fileURLWithPath(fullPath)
+                    
+                    let settings: [String: AnyObject] = [
+                        AVFormatIDKey:Int(kAudioFormatAppleIMA4), //Int required in Swift2
+                        AVSampleRateKey:44100.0,
+                        AVNumberOfChannelsKey:2,
+                        AVEncoderBitRateKey:12800,
+                        AVLinearPCMBitDepthKey:16,
+                        AVEncoderAudioQualityKey:AVAudioQuality.Max.rawValue
+                    ]
+                    
+                    try! self.audioRecorder = AVAudioRecorder(URL: url, settings: settings)
+                } else {
+                    print("not granted")
+                }
+            })
+        }
+
+    }
+    
     @IBAction func onDismissTap() {
         self.dismissViewControllerAnimated(true, completion: nil)
     }
@@ -46,6 +81,8 @@ class ContainerViewController: UIViewController {
         default:
             print("Somethin else")
         }
+    }
+    @IBAction func onRecordTap(sender: AnyObject) {
     }
 
     /*
